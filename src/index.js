af5fbb0f55020e2b072ded2ab89877902a29e43f
jison06 <ison.84@buckeyemail.osu.edu>
2020-09-19 21:46:19 -0400

    learned more from the tutorial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <div>Hi there!</div>;
+function getButtonText() {
+  return 'Click on me!';
+}
+
+const App = () => {
+  const btnStyle = { backgroundColor: 'red', color: 'white' };
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter Name:
+      </label>
+      <input id="name" type="text" />
+      <button style={btnStyle}>{getButtonText()}</button>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
